e62e94d22cd7397ab05d63637c6103f03b4ae8fd
Kevin Harrington <mad.hephaestus@gmail.com>
2019-03-06 10:16:41 -0500

    ading the full map

--- config.h
+++ config.h
@@ -11,23 +11,40 @@
 #define TEAM_NAME "IMU-Team21"
 
 #define USE_WIFI
+#define USE_IR_CAM
+#define USE_IMU
 
 #define WHEEL_TRACK 175.0
 #define WHEEL_RADIUS (2.0*25.4/2.0)
-
+/**
+ * Gripper pin for Servo
+ */
+#define SERVO_PIN 23
+/**
+ * Enable for all h-bridges
+ */
+#define H_BRIDGE_ENABLE             13  // emitter is controlled by digital pin
 // Pins
 /**
- * Drive motor 1 Servo PWM pin
+ * Drive motor 1 10Khz full duty PWM pin
  */
 #define MOTOR1_PWM 5
 /**
- * Drive motor 2 Servo PWM pin
+ * Pin for setting the direction of the H-Bridge
+ */
+#define MOTOR1_DIR 4
+/**
+ * Drive motor 2 10Khz full duty PWM pin
  */
 #define MOTOR2_PWM 15
 /**
+ * Pin for setting the direction of the H-Bridge
+ */
+#define MOTOR2_DIR 2
+/**
  * Drive motor 3 10Khz full duty PWM pin
  */
-#define MOTOR3_PWM 12
+#define MOTOR3_PWM 25
 /**
  * Pin for setting the direction of the H-Bridge
  */
@@ -41,17 +58,25 @@
 #define MOTOR2_ENCB 16
 
 #define MOTOR3_ENCA 14
-#define MOTOR3_ENCB 27
-// Line Sensor Pins
-#define LINE_SENSE_ONE 			36
-#define LINE_SENSE_TWO 			39
-#define EMITTER_PIN             13  // emitter is controlled by digital pin
+#define MOTOR3_ENCB 12
 
+//Stepper
+#define STEPPER_DIRECTION 33
+#define STEPPER_STEP      32
 
+// Sensor Pins
+#define ANALOG_SENSE_ONE		36
+#define ANALOG_SENSE_TWO		39
+#define ANALOG_SENSE_THREE		34
+#define ANALOG_SENSE_FOUR		35
 
-/**
- * Gripper pin for Servo
- */
-#define SERVO_PIN 23
+// Pins used by a perpheral, may be re-used
+#define BOOT_FLAG_PIN 			0
+#define I2C_SDA       			21
+#define I2C_SCL       			22
+#define SERIAL_PROGRAMMING_TX 	1
+#define SERIAL_PROGRAMMING_RX 	3
+#define WII_CONTROLLER_DETECT 	27
+
 
 #endif /* SRC_CONFIG_H_ */
